9f1eef7728fe26aa147bc3fe50e849c7f0aeda19
Kim Crayton <kmcrayton7@yahoo.com>
2015-10-06 10:22:05 -0400

    Added Greetr object

--- Greetr.js
+++ Greetr.js
@@ -1,6 +1,23 @@
 //Object: structuring safe code by creating an immediately invocted function
 (function(global, $) {
 	
-
+	var Greetr = function(firstName, lastName, language) {
+		return new Greetr.init(firstName, lastName, language);
+	}
+	
+//	Object: To set up a Greetr object using jQuery structure by creating a function constructor using .init
+	Greetr.prototype = {};
+	
+	Greetr.init = function(firstName, lastName, language) {
+		
+		var self = this;
+		self.firstName = firstName || "";
+		self.lastName = lastName || "";
+		self.language = language || "en";
+	}
+	
+	Greetr.init.prototype = Greetr.prototype;
+	
+	global.Greetr = global.G$ = Greetr;
 	
 }(window, jQuery));
