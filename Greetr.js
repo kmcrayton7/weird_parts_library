b4208c6a191b2f72cfd87ac80f6caacfd72d39f1
Kim Crayton <kmcrayton7@yahoo.com>
2015-10-06 11:20:17 -0400

    Added properties and chainable methods

--- Greetr.js
+++ Greetr.js
@@ -4,9 +4,70 @@
 	var Greetr = function(firstName, lastName, language) {
 		return new Greetr.init(firstName, lastName, language);
 	}
+//	Set up sahred objects of prototypes to save memory space
+	var supportedLangs = ["en", "es"];
+	
+//	Set up Greetings
+	var greetings = {
+		en: "Hello",
+		es: "Hola"
+	};
+	
+	var formalGreetings = {
+		en: "Greetings",
+		es: "Saludos"
+	};
+	
+	var logMessages = {
+		en: "Logged In",
+		es: "Inició sesión"
+	};
 	
 //	Object: To set up a Greetr object using jQuery structure by creating a function constructor using .init
-	Greetr.prototype = {};
+	Greetr.prototype = {
+		fullName: function() {
+			return this.firstName + " " + this.lastName;
+		},
+		validate: function() {
+			if (supportedLangs.indexOf(this.language) === -1) {
+				throw "Invalid Language";
+			}
+		},
+		greeting: function() {
+			return greetings[this.language] + " " + this.firstName + "!";
+		},
+		formalGreeting: function() {
+			return formalGreetings[this.language] + "," + this.fullName();
+		},
+		greet: function(formal) {
+			var msg;
+			
+//			if undefined or null it will be coerced to "false"
+			if(formal) {
+				msg = this.formalGreeting();
+			} else {
+				msg = this.greeting()
+			}
+			if(console) {
+				console.log(msg);
+			}
+//			"this" refers to the calling object at execution time which makes the method chainable
+			return this;
+		},
+		
+		log: function() {
+			if(console) {
+					console.log(logMessages[this.language] + ":" + this.fullName());
+			}
+			return this;
+		},
+ 		setLang: function(lang) {
+			this.language = lang;
+			this.validate();
+			return this;
+	
+		}
+	};
 	
 	Greetr.init = function(firstName, lastName, language) {
 		
